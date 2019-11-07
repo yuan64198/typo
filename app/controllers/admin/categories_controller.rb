eca3ecd16376218e3e6415413e27feccac32e021
EC2 Default User <ec2-user@ip-172-31-41-233.us-east-2.compute.internal>
2019-11-07 17:39:03 +0000

    more modify to new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,7 +25,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id]) ? Category.find(params[:id]) : Category.new
+    @category = (params[:id]) ? Category.find(params[:id]) : Category.new
     @category.attributes = params[:category]
     if request.post? and @category
       respond_to do |format|
